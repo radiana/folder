834d51159315dfe57c5672b798c731106ad043a6
unknown <User@User-PC.(none)>
2014-12-12 21:03:46 +0600

    Signed-off-by: unknown <User@User-PC.(none)>

---
+++ matrix multiplication using matrix.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int a[2][2] = {5,4,4,4};
+    int b[2][2] = {6,7,4,8};
+    int c[2][2] ,i,j,k,sum;
+
+    printf("matrix a is:\n\t\t");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+            printf("%d\t",a[i][j]);
+        printf("\n\t\t");
+    }
+     printf("matrix b is:\n\t\t");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+            printf("%d\t",b[i][j]);
+        printf("\n\t\t");
+    }
+    printf("\nmultiplication of a and b is:\n\t\t");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            sum = 0;
+            for(k=0;k<2;k++)
+                sum = sum + a[i][k] * b[k][j];
+            c[i][j] = sum;
+            printf("%d\t",c[i][j]);
+        }
+        printf("\n\t\t");
+    }
+    return 0;
+}
